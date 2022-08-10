be46a46f696ace8b03048cde644ca8cae7647fa0
Jayaraj <jayarajplusone@gmail.com>
2022-08-10 07:51:13 +0530

    Cleaned up _17_class.py
    Formatted document and output; Added some comments

--- code/_17_class.py
+++ code/_17_class.py
@@ -1,22 +1,28 @@
-print('Student Details')
+print('\nStudent Details\n---------------\n')
+
+
+# Without using class
 
 # first_student_id = 1
 # first_student_name = 'Anu'
 # first_student_status = 'Passed'
 # print('Details of student1')
-# print(f'Id :{first_student_id},Name :{first_student_name}, Status: {first_student_status}')
+# print(f'Id :{first_student_id}, Name :{first_student_name}, Status: {first_student_status}')
+
 # second_student_id = 2
 # second_student_name = 'Binu'
 # second_student_status = 'Passed'
 # print('Details of student2')
-# print(f'Id :{second_student_id},Name :{second_student_name}, Status: {second_student_status}')
+# print(f'Id :{second_student_id}, Name :{second_student_name}, Status: {second_student_status}')
 
 # third_student_id = 3
 # third_student_name = 'Elza'
 # third_student_status = 'Passed'
 # print('Details of student3')
-# print(f'Id :{third_student_id},Name :{third_student_name}, Status: {third_student_status}')
+# print(f'Id :{third_student_id}, Name :{third_student_name}, Status: {third_student_status}')
 
+
+# Using class
 
 class Student:
     def __init__(self, id: int, name: str, status: str):
@@ -31,6 +37,7 @@ class Student:
         if self.status=='Passed':
             return True
         return False
+
 
 s1 = Student(1, 'Anu', 'passed')
 s1.print_details()
@@ -47,3 +54,6 @@ print(s3.has_passed())
 s4 = Student(4, 'Divya', 'Passed')
 s4.print_details()
 print(s4.has_passed())
+
+# Problem with the above: 'status' is just a string; user of 'Student' class can instantiate it with invalid strings (other than 'Passed', 'Failed,' 'Pending')
+# Solution is to use an enum
