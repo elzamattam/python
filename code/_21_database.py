60e722d00e024999a2be738d94bd285e3b009bc2
Jayaraj <jayarajplusone@gmail.com>
2022-08-17 19:42:58 +0530

    Cleaned up _21_database.py file
    Formatted the code and added comments

--- code/_21_database.py
+++ code/_21_database.py
@@ -1,43 +1,32 @@
 import psycopg2
 
 
-connection = psycopg2.connect(host='localhost',port = 5432,user = 'postgres',password = 'postgres',database = 'postgres')
+# Connect to (PostgreSQL) database and get the connection object
+connection = psycopg2.connect(
+    host='localhost',
+    port = 5432,
+    user = 'postgres',
+    password = 'postgres',
+    database = 'postgres'
+)
+
+# Create the cursor object
 cursor = connection.cursor()
+
+# Execute SQL query - create table
 cursor.execute('create table if not exists fruit(id numeric,name varchar(50),colour varchar(50));')
+
+# Execute SQL query - insert row
 cursor.execute("insert into fruit(id,name,colour) values(1,'Apple','Red')")
+
+# Execute SQL query - get all rows
 cursor.execute('select * from fruit')
-rows = cursor.fetchall()
+rows = cursor.fetchall()  # Get the result of the previously executed query
 print(rows)
 
-
-
-
-
-
-
-
-
-
-
-
-
+# Commit the uncommitted transactions which change the database
 connection.commit()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Important! - Close the cursor object as well as connection object after use; better yet, put this in a finally block (and the code above in try block)
 cursor.close()
 connection.close()
