8cc13a971b6dcfe9ac66c5c6eb8a33b82c9138f9
elizabeth <elizabeth@strands01.local>
2022-08-18 13:14:31 +0530

    Created initial assignment code files
    Added code to create database if it does not exist

---
+++ code/_22_assignment/code/main.py
@@ -0,0 +1,37 @@
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
+electronic_database_name = 'electronic_devices_db'
+
+# Function to create database
+
+
+def create_connection_to_database(database_name: str):
+    conn = psycopg2.connect(
+        host='localhost',
+        port=5432,
+        user='postgres',
+        password='pokemon123',
+        database=database_name
+    )
+    conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    return conn
+
+
+# Connect to (PostgreSQL) database and get the connection object
+try:
+    conn = create_connection_to_database(electronic_database_name)
+    cursor = conn.cursor()
+
+except:
+    conn = create_connection_to_database('postgres')
+    cursor = conn.cursor()
+    cursor.execute("create database electronic_devices_db")
+    cursor.close()
+    conn.close()
+    conn = create_connection_to_database(electronic_database_name)
+    cursor = conn.cursor()
+
+conn.commit()
+cursor.close()
+conn.close()
